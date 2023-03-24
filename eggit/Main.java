0744d0394487a8573e29497dad5a2d8d37d168fa
David Ledesma <ledesma@outlook.com.ar>
2023-03-24 18:26:22 -0300

    Intellij - 6 Enough Data Types

--- eggit/Main.java
+++ eggit/Main.java
@@ -1,4 +1,8 @@
 package eggit;
+//To import a scanner we need this library, used to get input
+
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +12,7 @@ public class Main {
         //These are primitive data types AKA as not changeable
         int aSingleInteger= 5 ;
         double aSingleFloat = 5.1 ;
-        boolean aSingleBoolean = true ; ///or false lul
+        boolean aSingleBoolean = true ; //or false lul
         char aSingleChar = 'F'; ///A single string, Single quotation"
 
         //These date types are not primitive, AKA Changeable
@@ -21,5 +25,11 @@ public class Main {
         System.out.println("Tunnel Snakes rule? "+aSingleBoolean);
         System.out.println(aSingleChar);
         System.out.println(aSingleString);
+
+        Scanner aSingleUserInput = new Scanner(System.in);
+        String aSingleInput = aSingleUserInput.next(); ///If i expect an INT it would be .nextInt, a bool .nextBoolean, a double .nextDouble.
+        ///But the best way is to receive a string and cover it to the right data type".
+        System.out.println(aSingleInput);
+
     }
 }
