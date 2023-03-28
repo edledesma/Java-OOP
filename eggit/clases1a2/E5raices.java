df693163f9570d1605b552a472f6172c11001f70
David Ledesma <ledesma@outlook.com.ar>
2023-03-27 22:30:35 -0300

    JAVA - Clase 1/2 - Ejercicios V2

--- eggit/clases1a2/E5raices.java
+++ eggit/clases1a2/E5raices.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 
 public class E5raices {
     public static void main(String[] args) {
-
+        System.out.print("Input a value: ");
         Scanner valorDeIngreso = new Scanner(System.in) ;
         int intA = valorDeIngreso.nextInt();
 
-        System.out.println("The double of "+ intA + " is " + (Math.pow(intA,2)));
-        System.out.println("The triple of "+ intA + " is " + (Math.pow(intA,3)));
+        System.out.println(intA +" to the power of 2 is " + (Math.pow(intA,2)));
+        System.out.println(intA +" to the power of 3 is " + (Math.pow(intA,3)));
         System.out.println("The root square of "+ intA + " is " + (Math.sqrt(intA)));
     }
 }
