ba5b072c9a7a0786185e595de5a100bc8016eb96
David Ledesma <ledesma@outlook.com.ar>
2023-03-30 21:44:29 -0300

    JAVA - Clase 4/6 - Ejercicios v2

--- eggit/clases4a6/E7rs.java
+++ eggit/clases4a6/E7rs.java
@@ -1,4 +1,7 @@
 package eggit.clases4a6;
+
+import java.util.Scanner;
+
 /*Realizar un programa que simule el funcionamiento de un dispositivo RS232, este tipo de dispositivo lee cadenas enviadas por el usuario.
 Las cadenas deben llegar con un formato fijo: tienen que ser de un máximo de 5 caracteres de largo, el primer carácter tiene que ser X y el último tiene que ser una O.
 Las secuencias leídas que respeten el formato se consideran correctas, la secuencia especial “&&&&&” marca el final de
@@ -8,8 +11,27 @@ Para resolver el ejercicio deberá investigar cómo se utilizan las siguientes f
 */
 public class E7rs {
     public static void main(String[] args) {
+        Scanner aNewScan = new Scanner(System.in);
+        String aPhrase;
+        int cValid, cInvalid;
+        cValid = 0;
+        cInvalid= 0;
+        do {
 
+           System.out.print("Input a new phrase: ");
+           aPhrase = aNewScan.next();
+           if ((aPhrase.length()==5) && (aPhrase.substring(0, 1).equals("X")) &&(aPhrase.substring(4, 5).equals("O"))  &&  (!aPhrase.equals("&&&&&")))  {
+                System.out.println("This is valid");
+                cValid++;
+           }else{
+                System.out.println("This is not valid");
+               cInvalid++;
+            }
 
+        }
+        while (!aPhrase.equals("&&&&&"));
+        System.out.println("There was total of " + cValid + " valid reads.");
+        System.out.println("There was total of " + cInvalid + " non valid reads.");
 
     }
 
